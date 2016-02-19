8d619f97683a27f9388f38835c0173ff0b4627e5
Sachin Pandit <sachinspandits@gmail.com>
2016-02-19 00:32:30 -0800

    Coding: Zero fill the matrix

--- matrix_zero.java
+++ matrix_zero.java
@@ -10,32 +10,45 @@ public class matrix_zero {
                 }
         }
 
-        public static void fill_zero (int matrix[][], int row, int column) {
+        public static void null_row (int matrix[][], int row) {
                 for (int i = 0; i < 4; i++) {
                         matrix[row][i] = 0;
+                }
+        }
+
+        public static void null_column (int matrix[][], int column) {
+                for (int i = 0; i < 4; i++) {
                         matrix[i][column] = 0;
                 }
         }
 
         public static void main (String[] str) {
                 int matrix[][] = {{1,2,0,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-                int final_matrix[][] = new int[4][4];
-                System.out.println ("Before:");
-                for (int i = 0; i < 4; i++) {
-                        for (int j = 0; j < 4; j++) {
-                                final_matrix[i][j] = matrix[i][j];
-                        }
-                }
+                boolean[] row = new boolean[matrix.length];
+                boolean[] column = new boolean[matrix[0].length];
+
                 print_matrix (matrix);
-                for (int i = 0; i < 4; i++) {
-                        for (int j = 0; j < 4; j++) {
+                for (int i = 0; i < matrix.length; i++) {
+                        for (int j = 0; j < matrix.length; j++) {
                                 if (matrix[i][j] == 0) {
-                                        fill_zero (final_matrix, i, j);
+                                        row[i] = true;
+                                        column[j] = true;
                                 }
                         }
                 }
-                System.out.println ("After:");
-                print_matrix (final_matrix);
+
+                for (int i = 0; i < row.length; i++) {
+                        if (row[i]) {
+                                null_row (matrix, i);
+                        }
+                }
+
+                for (int j = 0; j < column.length; j++) {
+                        if (column[j]) {
+                                null_column (matrix, j);
+                        }
+                }
+                print_matrix (matrix);
 
         }
 }
